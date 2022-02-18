1e851c5ba1f917f93ec3172e94c27f27fb0504e3
Noelia Parada <noeliacastrelo@gmail.com>
2022-02-18 18:03:16 +0100

    iteracción 1 hecha

---
+++ models/Movie.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    director: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    showtimes: {
+        type: [String]
+    }
+})
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie
